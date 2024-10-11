8fe3fdec49a44f6c901c62ec376b3a8a88fb5869
acciojob <akshay121817@gmail.com>
2024-10-11 23:12:04 +0530

    Polymorphism added with fixed values

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,4 +2,11 @@ package com.driver;
 
 public class Main {
 
+    public static void main(String[] args) {
+        Product p = new Product();
+        p.product(2, 3);
+        p.product(2, 3, 4);
+        p.product(2.5, 2.5);
+    }
+
 }
